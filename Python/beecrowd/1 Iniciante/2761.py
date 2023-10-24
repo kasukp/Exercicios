94338c0ea290ba17b0cb0e5fcd9b6f5c58b5d8cf
Gabriel Salim Carneiro <8004844+kasukp@users.noreply.github.com>
2023-10-24 09:08:58 -0300

    Update 2761.py

--- Python/beecrowd/1 Iniciante/2761.py
+++ Python/beecrowd/1 Iniciante/2761.py
@@ -1,8 +1,11 @@
-import numpy as np
+# Beecrowd não suporta numpy
+# import numpy as np
 
 var = input().split(" ", maxsplit=3)
-# var[1] = float(var[1])
-var[1] = np.float32(var[1])
+
+# Exercício exige 32-bits float
+# var[1] = np.float32(var[1])
+var[1] = float(var[1])
 
 print(f"{var[0]}{var[1]:6f}{var[2]}{var[3]}")
 print(f"{var[0]}\t{var[1]:.6f}\t{var[2]}\t{var[3]}")
